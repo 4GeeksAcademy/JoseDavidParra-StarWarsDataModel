9a9fae3c1370a714c7c57885499c79f1f0ead07e
josiedpb14 <110139188+josiedpb14@users.noreply.github.com>
2023-07-19 10:20:56 +0000

    DONE 1000%

--- src/models.py
+++ src/models.py
@@ -15,9 +15,9 @@ class User(Base):
     username = Column(String(250), nullable=False)
     email = Column(String(250))
     password = Column(String(250))
-    favorite_planets = relationship('Planets',backref = 'user',lazy=True)
-    favorite_characters = relationship('Characters',backref = 'user' ,lazy = True)
-    favorite_starships = relationship('Starships',backref = 'user' ,lazy = True)
+    favorite_planets = relationship('FavoritePlanets',backref = 'user',lazy=True)
+    favorite_characters = relationship('FavoriteCharacters',backref = 'user' ,lazy = True)
+    favorite_starships = relationship('FavoriteStarships',backref = 'user' ,lazy = True)
     
 class Planets(Base):
     __tablename__ = 'planets'
@@ -31,7 +31,7 @@ class Planets(Base):
     terrain = Column(String)
     surface_water = Column(Integer)
     population = Column(Integer)
-    user_id = Column(Integer,ForeignKey('user.id'))
+    usersPlanets = relationship('FavoritePlanets',backref = 'plantes',lazy=True)
     characters = relationship("Characters",backref = "planets",lazy =True)
 
 class Characters(Base):
@@ -46,7 +46,7 @@ class Characters(Base):
     birth_year = Column(Integer)
     gender = Column(String)
     planet_id = Column(Integer,ForeignKey('planets.id'))
-    user_id = Column(Integer,ForeignKey('user.id'))
+    usersCharacters = relationship('FavoriteCharacters',backref = 'characters',lazy=True)
     starships = relationship('Pilots',backref = 'characters' ,lazy = True)
 
 class Starships(Base):
@@ -65,13 +65,28 @@ class Starships(Base):
     hyperdrive_rating = Column(REAL)
     mglt = Column(Integer)
     starship_class = Column(String)
-    user_id = Column(Integer,ForeignKey('user.id'))
+    usersStarships = relationship('FavoriteStarships',backref = 'starships',lazy=True)
     pilots = relationship('Pilots',backref = 'starships' ,lazy = True)
 
 class Pilots(Base):
     __tablename__ = 'pilots'
     pilot_id = Column(Integer,ForeignKey('characters.id'),primary_key = True)
     starship_id = Column(Integer,ForeignKey('starships.id'),primary_key = True)
+
+class FavoritePlanets(Base):
+    __tablename__ = 'favorite_planets'
+    user_id = Column(Integer,ForeignKey('user.id'),primary_key = True)
+    planet_id = Column(Integer,ForeignKey('planets.id'),primary_key = True)
+
+class FavoriteCharacters(Base):
+    __tablename__ = 'favorite_characters'
+    user_id = Column(Integer,ForeignKey('user.id'),primary_key = True)
+    character_id = Column(Integer,ForeignKey('characters.id'),primary_key = True)
+
+class FavoriteStartchips(Base):
+    __tablename__ = 'favorite_starships'
+    user_id = Column(Integer,ForeignKey('user.id'),primary_key = True)
+    startship_id = Column(Integer,ForeignKey('starships.id'),primary_key = True)
     
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
