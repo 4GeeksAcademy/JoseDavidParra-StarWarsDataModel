e0170eb3aa694d755defe512f116982c90b96f89
josiedpb14 <110139188+josiedpb14@users.noreply.github.com>
2023-07-24 10:03:17 +0000

    Done still

--- src/models.py
+++ src/models.py
@@ -68,6 +68,11 @@ class Starship(Base):
     usersStarship = relationship('FavoriteStarship',backref = 'starships',lazy=True)
     pilots = relationship('Pilots',backref = 'starships' ,lazy = True)
 
+# class Favorite(Base):
+#     __tablename__ = "favorites"
+#     id = Column(Integer,primary_key=True)
+#     items = relationship()
+
 class Pilots(Base):
     __tablename__ = 'pilots'
     pilot_id = Column(Integer,ForeignKey('characters.id'),primary_key = True)
